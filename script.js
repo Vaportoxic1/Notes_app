bf1af9c6df6567caa2aa9d6ddb71ae56962be68d
Vaportoxic1 <kayitarepatrick06@gmail.com>
2023-10-02 18:05:52 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const notescontainer  = document.querySelector(".notes-container");
+const createbtn = document.querySelector(".btn");
+let notes = document.querySelector(".input-box");
+
+function showNotes(){
+    notescontainer.innerHTML = localStorage.getItem("notes");
+}
+function updatestorage(){
+    localStorage.setItem("notes", notescontainer.innerHTML);
+}
+
+showNotes();
+createbtn.addEventListener("click", ()=>{
+    let inputbox = document.createElement("p");
+    let img = document.createElement("img");
+    inputbox.className = "input-box";
+    inputbox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png";
+    notescontainer.appendChild(inputbox).appendChild(img);
+});
+
+notescontainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updatestorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt =>{
+            nt.onkeyup = function(){
+                updatestorage();
+            }
+        });
+    }
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+});
